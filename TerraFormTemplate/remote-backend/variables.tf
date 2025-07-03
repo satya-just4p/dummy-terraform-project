6ecff85d5f5aaa12d38c637776d9e0297f98f5ad
Satyapriya I <satyajust4p@gmail.com>
2025-07-03 22:43:42 +0200

    updated the remote-backend terraform file to include the bucket name and dynamodb table

--- TerraFormTemplate/remote-backend/variables.tf
+++ TerraFormTemplate/remote-backend/variables.tf
@@ -1,11 +1,13 @@
 variable "bucket_name"{
     description = "Name of the S3 bucket for storing the terraform state"
+    default = "dummy-tfstate-bucket"
     type = string 
 }
 
 variable "lock_table_name"{
     description = "Name of the DynamoDB Table name for State locking "
     type = string
+    default = "tfstate-lock-table"
 }
 
 variable "environment"{
