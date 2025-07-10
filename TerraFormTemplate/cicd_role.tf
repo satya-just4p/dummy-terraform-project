e0bf81bf79bc35953f52e7f95932f72fcc8dfb6a
Satyapriya I <satyajust4p@gmail.com>
2025-07-10 22:31:32 +0200

    created a new role for Angular CI/CD Pipeline. Made neccessary changes in outputs.tf, infrastructure.yml and frontend.yml files

---
+++ TerraFormTemplate/cicd_role.tf
@@ -0,0 +1,65 @@
+# Define the OIDC Provider
+resource "aws_iam_openid_connect_provider" "github"{
+    url = "https://token.actions.githubusercontent.com"
+    clientid_list = ["sts.amazonaws.com"]
+    thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"] # GitHub's root Cert
+}
+
+# Create IAM Role for GitHub actions
+resource "aws_iam_role" "github_actions_role"{
+    name = "github-actions-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [{
+            Effect = "Allow",
+            Principal = {
+                Federated = aws_iam_openid_connect_provider.github.arn
+            },
+            Action = "sts:AssumeRoleWithWebIdentity",
+            Condition = {
+                stringEquals = {
+                    "token.actions.githubusercontent.com:sub" = "repo:satya-just4p/dummy-terraform-project:ref:refs/heads/main"
+                }
+            }
+        }]
+    })
+}
+
+# Attach Inline Policies to the role
+resource "aws_iam_role_policy" "cicid_user_inline_policy"{
+    name = "cicd-user-inline-policy"
+    role = aws_iam_role.github_actions_role.id
+
+    policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [{
+            Effect = "Allow",
+        Action = [
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:DeleteObject",
+            "s3:ListBucket"
+
+        ],
+        Resource = [
+            "arn:aws:s3::${var.angular_s3_bucket_name}",
+            "arn:aws:s3::${var.angular_s3_bucket_name}/*"
+        ]
+        },
+        {
+            Effect = "Allow",
+            Action = [
+                "cloudfront:CreateInvalidation",
+                "cloudfront:CreateInvalidationForDistributionTenant"
+            ],
+            Resource = ["*"]
+        }
+        
+        ]
+        
+    })
+
+}
+ 
--- TerraFormTemplate/outputs.tf
+++ TerraFormTemplate/outputs.tf
@@ -1,7 +1,3 @@
-output "project_region"{
-    description = "Project Region"
-    value = var.aws_region
-}
 output "s3_bucket_name"{
     description = "S3 bucket Name"
     value = aws_s3_bucket.angular_bucket.bucket
@@ -9,46 +5,6 @@ output "s3_bucket_name"{
 output "angular_cloudfront_domain_name"{
     description = "Cloudfront domain name for Angular frontend"
     value = aws_cloudfront_distribution.angular_cdn.domain_name
-}
-output "angular_cloudfront_distribution_id"{
-    description = "CloudFront Distribution Id"
-    value = aws_cloudfront_distribution.angular_cdn.id
-}
-output "vpc_id"{
-    description = "VPC ID for Dummy Project"
-    value = aws_vpc.dummy_vpc.id
-}
-output "public_subnet_id"{
-    description = "Public Subnet ID for Bastion Host"
-    value = aws_subnet.dummy_public_subnet.id
-}
-output "private_subnet_id"{
-    description = "Private Subnet ID for RDS and AWS Lambda"
-    value = aws_subnet.dummy_private_subnet.id
-}
-output "bastion_dummy_sg_id"{
-    description = "Security Group ID for Bastion Host"
-    value = aws_security_group.bastion_dummy_sg.id
-}
-output "dummy_bastion_host_ip"{
-    description = "Public IP of the Bastion Host for SSH Access"
-    value = aws_instance.dummy_bastion_instance.public_ip
-}
-
-# output "bastion_ssh_command"{
-#    description = "SSH command to connect to the Bastion Host"
-#    value = "ssh -i ${tls_private_key.bastion_key.private_key_pem} ec2-user@${aws_instance.dummy_bastion_instance.public_ip}"
-#    sensitive = true
-#}
-
-output "rds_endpoint"{
-    description = "Amazon RDS endpoint for database connection"
-    value = aws_db_instance.dummy_db_instance.endpoint
-}
-
-output "rds_address"{
-    description = "Amazon RDS endpoint for database connection without the port"
-    value = aws_db_instance.dummy_db_instance.address
 }
 
 output "lambda_function_name"{
@@ -60,25 +16,80 @@ output "lambda_function_url"{
     value = aws_lambda_function_url.dummy_lambda_function_url.function_url
     sensitive = false
 }
-
 output "angular_base_api_url"{
     description = "Base API URL for Angular app to call API gateway"
     value = aws_apigatewayv2_stage.dummy_stage.invoke_url
     sensitive = false
 }
 
-# Access Key and Secret Access Key output
-output "cicd_user_access_key_id"{
-    value = aws_iam_access_key.cicd_user_keys.id
-    description = "Access key ID for CI/CD user"
-    sensitive = true
+output "github_actions_role_arn"{
+    description = "IAM Role for GitHub Actions for Angular CI/CD Pipeline"
+    value = aws_iam_role.github_actions_role.arn
 }
 
-output "cicd_user_secret_access_key"{
-    value = aws_iam_access_key.cicd_user_keys.secret
-    description = "Secret Access Key ID for CI/CD User"
-    sensitive = true
+output "project_region"{
+    description = "Project Region"
+    value = var.aws_region
 }
+
+output "angular_cloudfront_distribution_id"{
+    description = "CloudFront Distribution Id"
+    value = aws_cloudfront_distribution.angular_cdn.id
+}
+
+# Commented URL used only for Local Testing
+
+
+#output "vpc_id"{
+#    description = "VPC ID for Dummy Project"
+#    value = aws_vpc.dummy_vpc.id
+#}
+#output "public_subnet_id"{
+#    description = "Public Subnet ID for Bastion Host"
+#    value = aws_subnet.dummy_public_subnet.id
+#}
+#output "private_subnet_id"{
+#    description = "Private Subnet ID for RDS and AWS Lambda"
+#    value = aws_subnet.dummy_private_subnet.id
+#}
+#output "bastion_dummy_sg_id"{
+#    description = "Security Group ID for Bastion Host"
+#    value = aws_security_group.bastion_dummy_sg.id
+#}
+output "dummy_bastion_host_ip"{
+    description = "Public IP of the Bastion Host for SSH Access"
+    value = aws_instance.dummy_bastion_instance.public_ip
+}
+
+# output "bastion_ssh_command"{
+#    description = "SSH command to connect to the Bastion Host"
+#    value = "ssh -i ${tls_private_key.bastion_key.private_key_pem} ec2-user@${aws_instance.dummy_bastion_instance.public_ip}"
+#    sensitive = true
+#}
+
+#output "rds_endpoint"{
+#    description = "Amazon RDS endpoint for database connection"
+#    value = aws_db_instance.dummy_db_instance.endpoint
+#}
+
+#output "rds_address"{
+#    description = "Amazon RDS endpoint for database connection without the port"
+#    value = aws_db_instance.dummy_db_instance.address
+#}
+
+
+# Access Key and Secret Access Key output
+#output "cicd_user_access_key_id"{
+#    value = aws_iam_access_key.cicd_user_keys.id
+#    description = "Access key ID for CI/CD user"
+#    sensitive = true
+#}
+
+#output "cicd_user_secret_access_key"{
+#    value = aws_iam_access_key.cicd_user_keys.secret
+#    description = "Secret Access Key ID for CI/CD User"
+#    sensitive = true
+#}
 
 #output "bastion_private_key"{
 #    value = tls_private_key.bastion_key.private_key_pem
